d9f71c773441404c36813fd298b3d0af71308422
seslone <seslone@gmail.com>
2021-06-02 09:17:01 -0400

    more additions to maze + maze test

--- classic_cs_problems/chapter_2/generic_search.py
+++ classic_cs_problems/chapter_2/generic_search.py
@@ -24,6 +24,16 @@ def Comparable(Protocol):
     return not self < other
 
 
+class Node(Generic[T]):
+  def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
+    self.state: T = state
+    self.parent: Optional[Node] = parent
+    self.cost: float = cost
+    self.heuristic: float = heuristic
+
+  def __lt__(self, other: Node) -> bool:
+    return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+
 def linear_contains(iterable: Iterable[T], key: T) -> bool:
   for item in iterable:
     if item == key:
@@ -42,3 +52,19 @@ def binary_contains(sequence: Sequence[C], key: C) -> bool:
     else:
       return True
   return False
+
+
+def dfs():
+  pass
+
+
+def bfs():
+  pass
+
+
+def node_to_path():
+  pass
+
+
+def astar():
+  pass
